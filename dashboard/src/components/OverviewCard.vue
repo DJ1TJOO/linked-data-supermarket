3861c5fe719c527e39382149c91d769f0c219ba5
Thomas (DJ1TJOO) <t.ferb1@gmail.com>
2026-04-05 20:59:02 +0200

    feat: components

---
+++ dashboard/src/components/OverviewCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Card, CardContent } from './ui/card';
+
+defineProps<{
+    title: string;
+    value: string;
+    type: "default" | "danger" | "success";
+}>()
+
+const textColor = {
+    default: "text-gray-800",
+    danger: "text-red-600",
+    success: "text-green-600",
+}
+</script>
+
+<template>
+    <Card>
+        <CardContent class="py-6 flex flex-col items-center">
+            <div class="text-lg font-semibold text-muted-foreground">{{ title }}</div>
+            <div class="text-3xl font-bold mt-2" :class="textColor[type]">{{ value }}</div>
+        </CardContent>
+    </Card>
+</template>
